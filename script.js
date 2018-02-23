180a259ad8270362fa60f94f2f8594a3f564a1fa
Jason Mattis <rocknrollx18@aim.com>
2018-02-22 21:23:45 -0800

    Added two difficutlies. Easy and Hard. When eas is selected. It will display three colors. When hard is selected, it will display six squares.

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
 var colors = generateRandomColors(6);
-
+var numSquares = 6;
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickRandomColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyButton = document.querySelector("#easyBtn");
+var hardButton = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function() {
+  easyBtn.classList.add("selected");
+  hardBtn.classList.remove("selected");
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickRandomColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++) {
+    if(colors[i]) {
+      squares[i].style.background = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+});
+
+hardBtn.addEventListener("click", function() {
+  hardBtn.classList.add("selected");
+  easyBtn.classList.remove("selected");
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickRandomColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
+    squares[i].style.display = "block";
+  }
+});
 
 colorDisplay.textContent = pickedColor;
 
 resetButton.addEventListener("click", function() {
   resetButton.textContent = "New Colors";
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
   pickedColor = pickRandomColor();
   colorDisplay.textContent = pickedColor;
   for (var i = 0; i < squares.length; i++) {
